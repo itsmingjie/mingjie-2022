600c69fd5881aa3ef8e8eb3de640098b68a2cbac
Mingjie Jiang <m6@berkeley.edu>
2022-02-06 09:39:59 -0500

    add spinner

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const NowPlaying = () => {
       );
       const lastfm_data = await lastfm_response.json();
       if (!lastfm_data.error) {
-        setMusic(lastfm_data["recenttracks"]["track"][0]);
+        // setMusic(lastfm_data["recenttracks"]["track"][0]);
       }
     }
 
@@ -26,8 +26,8 @@ const NowPlaying = () => {
 
   return (
     <>
-      {music && (
-        <p className="mt-3 animated-fadeup anim-delay-2">
+      {music ? (
+        <p className="mt-3">
           {music.artist["#text"].includes("Taylor Swift") ? (
             <span>
               I listen to Taylor Swift a lot. In fact, my most recently played
@@ -45,6 +45,25 @@ const NowPlaying = () => {
             </span>
           )}
         </p>
+      ) : (
+        <>
+          <svg
+            role="status"
+            class="mt-3 w-5 h-5 text-pink animate-spin dark:text-pink fill-coral"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+        </>
       )}
     </>
   );
@@ -61,13 +80,13 @@ const Home = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex justify-between animated-fadeup">
+      <div className="flex justify-between">
         <div className="font-bold">Mingjie Jiang</div>
         <Link href="https://twitter.com/itsmingjie" external>
           <div className="font-serif text-coral font-bold">@itsmingjie</div>
         </Link>
       </div>
-      <div className="mt-5 animated-fadeup anim-delay-2">
+      <div className="mt-5">
         <p>
           Learner & builder focusing on education. Kind of dropped out
           <Link
@@ -83,11 +102,11 @@ const Home = () => {
               <div className="inline font-bold text-coral">Ender</div>
             </Link>
           </div>
-          &mdash; building what I have always wanted in education for the next
-          generation of creative kids who love to build.
+          &mdash; helping to build what I would've wanted in education for the
+          next generation of creative kids who love to build.
         </p>
         <NowPlaying />
-        <p className="animated-fadeup anim-delay-3 mt-3 text-hint-1 font-serif italic">
+        <p className="mt-3 text-hint-1 font-serif italic">
           A work in progress website. In the meantime, you should check out{" "}
           <Link href="https://mingjie.notion.site/" external>
             <div className="text-coral inline">things I wrote about</div>
