d992b091ed22127479c618d14883d75468c0d272
Mingjie Jiang <jiang@mingjie.dev>
2022-04-16 15:43:55 -0500

    add analytics

---
+++ design-system/utils/insights/insights.tsx
@@ -0,0 +1,8 @@
+export const Insights = () => (
+  <script
+    async
+    defer
+    data-website-id='6f160b20-0244-4529-8fd2-9718187013d4'
+    src='https://insights.mingjie.dev/x.js'
+  ></script>
+);
