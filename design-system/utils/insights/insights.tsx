97304f5ee6c0bacf6b0b58cf5be1ed8084344f84
Mingjie Jiang <jiang@mingjie.dev>
2022-04-17 17:25:22 -0500

    add umami-specific configs

--- design-system/utils/insights/insights.tsx
+++ design-system/utils/insights/insights.tsx
@@ -4,5 +4,7 @@ export const Insights = () => (
     defer
     data-website-id='6f160b20-0244-4529-8fd2-9718187013d4'
     src='https://insights.mingjie.dev/x.js'
+    data-do-not-track='true'
+    data-domains='mingjie.dev'
   ></script>
 );
