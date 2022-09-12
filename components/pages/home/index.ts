8e27a819c9d204e14c671eb971e522ce4d9be95c
Mingjie Jiang <jiang@mingjie.dev>
2022-09-12 02:12:25 -0500

    update website

--- components/pages/home/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './now-playing/now-playing';
-export * from './recent-posts/recent-posts';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,74 +1,96 @@
-import { NowPlaying, RecentPosts } from '../components/pages/home';
 import { Header } from '../components/header';
 import Link from 'next/link';
-import { getDatabase } from '../libs/notion';
 import { Paragraph } from '../design-system/components/paragraph';
 import { SingleLayout } from '../design-system/layouts/single-layout';
 import { Section } from '../design-system/components/section';
 import { Meta } from '../design-system/utils/meta';
+import * as S from './index.styles';
 
-const Home = ({ posts }: { posts: any[] }) => {
+const Home = () => {
   return (
     <SingleLayout>
       <Meta />
 
-      <Header title='Mingjie Jiang' subtitle='Web & Product Engineer' />
+      <S.Container>
+        <Header title='Mingjie Jiang' subtitle='Web & Product Engineer' />
 
-      <Paragraph>
-        Learner & builder focusing on education. Immigrant born and raised in
-        Shanghai, China. Dropped out from UC Berkeley CS to help build{' '}
-        <Link href='https://joinender.com'>Ender</Link>, crafting what I
-        would&rsquo;ve wanted in education for the next generation of creative
-        kids who love to build.
-      </Paragraph>
+        <Section title='About'>
+          <Paragraph>
+            Learner & builder, crafting interactive education experiences for
+            kids at <Link href='https://joinender.com'>Ender</Link>.
+          </Paragraph>
 
-      <Paragraph>
-        You might be interested some interesting{' '}
-        <Link href='/spellings'>spellings</Link> of my name.
-      </Paragraph>
+          <Paragraph>
+            I grew up in Shanghai, immigrated to Maryland, and went to UC
+            Berkeley for a little over a year. I now live in Austin, a weird yet
+            beautiful, diverse, and fast-growing city.
+          </Paragraph>
 
-      <Section title='Notes'>
-        I manage my knowledge in Obsidian, and some of them is public.{' '}
-        <Link href={'/notes/Notes - Home'}>See my notes &rarr;</Link>
-      </Section>
+          <Paragraph>
+            I spent a few years working at CS education nonprofits like{' '}
+            <Link href='https://hackclub.com' passHref>
+              <a target='_blank' rel='noopener noreferrer'>
+                Hack Club
+              </a>
+            </Link>
+            ,{' '}
+            <Link href='https://codeday.org/' passHref>
+              <a target='_blank' rel='noopener noreferrer'>
+                CodeDay
+              </a>
+            </Link>
+            , and eventually cofounded my own initiative{' '}
+            <Link href='https://executebig.org/' passHref>
+              <a target='_blank' rel='noopener noreferrer'>
+                Execute Big
+              </a>
+            </Link>
+            . I enjoy teaching, building, and doing research about education.
+          </Paragraph>
+        </Section>
 
-      {/* <Section title='Other Involvements'>
-        <Paragraph>
-          Executive Director of{' '}
-          <Link href='https://calhacks.io'>Cal Hacks</Link> (my last obligation
-          at Berkeley), building the world&rsquo;s largest collegiate hackathon
-          and a few peripheral projects supporting the school&rsquo;s
-          entrepreneurial culture.
-        </Paragraph>
-        <Paragraph>
-          Cofounder of <Link href='https://executebig.org'>Execute Big</Link>, a
-          non-profit organization that supports underrepresented students on
-          their journey to explore computer science, specifically tackling
-          problems often neglected by larger organizations.
-        </Paragraph>
-      </Section> */}
+        <Section title='Brain'>
+          I started managing my knowledge publicly in Notion.{' '}
+          <Link href='https://mingjie.notion.site' passHref>
+            <a target='_blank' rel='noopener noreferrer'>
+              See my brain &rarr;
+            </a>
+          </Link>
+        </Section>
 
-      <Section title='Journals'>
-        <RecentPosts posts={posts} />
-      </Section>
+        <Section title='Links'>
+          <Paragraph>
+            <S.LinksList>
+              <Link href='https://twitter.com/itsmingjie' passHref>
+                <a target='_blank' rel='noopener noreferrer'>
+                  Twitter
+                </a>
+              </Link>
 
-      <Section title='Recently Played'>
-        <NowPlaying />
-      </Section>
+              <S.Separator decorative orientation='vertical' />
+
+              <Link href='https://github.com/itsmingjie' passHref>
+                <a target='_blank' rel='noopener noreferrer'>
+                  GitHub
+                </a>
+              </Link>
+
+              <S.Separator decorative orientation='vertical' />
+
+              <Link
+                href='https://scholar.google.com/citations?user=J9sYVMEAAAAJ&hl'
+                passHref
+              >
+                <a target='_blank' rel='noopener noreferrer'>
+                  Scholar
+                </a>
+              </Link>
+            </S.LinksList>
+          </Paragraph>
+        </Section>
+      </S.Container>
     </SingleLayout>
   );
-};
-
-export const getStaticProps = async () => {
-  const databaseId = process.env.NOTION_BLOG_DB as string;
-  const database = await getDatabase(databaseId);
-
-  return {
-    props: {
-      posts: database.slice(0, 5),
-    },
-    revalidate: 1, // In seconds
-  };
 };
 
 export default Home;
