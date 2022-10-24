7c9f6bbd9eff42970f97d4f9fea7a6ab00180460
Mingjie Jiang <jiang@mingjie.dev>
2022-10-23 23:11:50 -0500

    new font: Archivo (#15)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href='https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital@0;1&family=IBM+Plex+Sans+Condensed:ital@0;1&family=IBM+Plex+Sans:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&family=IBM+Plex+Serif:ital@0;1&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Archivo+Black&family=IBM+Plex+Mono:ital@0;1&family=Archivo+Narrow:ital@0;1&family=Archivo:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap'
           rel='stylesheet'
         />
         <ThemeStyles />
